cbd1838c9982fe097b93a1c2f61175016664f4ee
rajKarra69420 <60681803+rajKarra69420@users.noreply.github.com>
2020-07-27 14:34:30 -0400

    Add files via upload

--- SocialNetworkMessaging/src/Client.java
+++ SocialNetworkMessaging/src/Client.java
@@ -5,20 +5,16 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Client {
-	
-	public final int WINDOW_WIDTH = 800;
-	public final int WINDOW_HEIGHT = 600;
-	public String[] PANEL_CHOICES = new String[] {"Login Panel", "Chat List Panel", "Chat Panel"};
-	
-	private JFrame frame;
-	private JPanel contentPane;
-	
-	private JPanel clientLoginPanel;
-	
-	public Client() {
-		
-	}
 
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    Socket s;
+
+    public Client(Socket s) throws IOException{
+        this.s = s;
+        this.out = new ObjectOutputStream(s.getOutputStream());
+        this.in = new ObjectInputStream(s.getInputStream());
+    }
     public boolean verifySignIn(Packet p) {
         return p.isVerified();
     }
@@ -38,42 +34,21 @@ public class Client {
     public boolean verifyChangeChatName(Packet p) {
         return p.isVerified();
     }
-    
-    public static void main(String[] args) throws IOException {
-    	//Socket socket = new Socket("127.0.0.1", 5555); //handle this later
-    	//BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    	//PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        //JOptionPane.showMessageDialog(null, "Welcome to WSC", "WSC", JOptionPane.INFORMATION_MESSAGE);
-    	
-    	SwingUtilities.invokeLater(new Runnable()
-        {
-            public void run()
-            {
-            	new Client().displayGUI();
-            }
-        });
-    }
-    
-    public JPanel getContentPane() {
-    	return this.contentPane;
-    }
-    
-    private void displayGUI()
-    {
-        JFrame frame = new JFrame("Card Layout Example");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel contentPane = new JPanel();
-        contentPane.setBorder(
-            BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new CardLayout());  
-        
-        clientLoginPanel = new ClientLoginGUI(this.WINDOW_WIDTH, this.WINDOW_HEIGHT);
-        contentPane.add(clientLoginPanel, this.PANEL_CHOICES[0]);
-        
-        frame.getContentPane().add(contentPane, BorderLayout.CENTER);       
-        frame.pack();   
-        frame.setLocationByPlatform(true);
-        frame.setVisible(true);
+    public void sendPacket(Packet p) throws IOException{
+        this.out.writeObject(p);
     }
+
+    public Packet recivePacket() throws IOException, ClassNotFoundException{
+        return (Packet) this.in.readObject();
+    }
+
+    public void sendUser(User u) throws IOException{
+        this.out.writeObject(u);
+    }
+
+    public User reciveUser() throws IOException, ClassNotFoundException{
+        return (User) this.in.readObject();
+    }
+
 }
