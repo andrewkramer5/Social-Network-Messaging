83557561336e5346b058b59960d25fae0f491ffe
rajKarra69420 <60681803+rajKarra69420@users.noreply.github.com>
2020-07-27 23:37:48 -0400

    Add files via upload

--- SocialNetworkMessaging/src/User.java
+++ SocialNetworkMessaging/src/User.java
@@ -18,10 +18,6 @@ public class User implements Serializable {
         chats = new ArrayList<>();
         numFriends = 0;
     }
-    
-    public User() {
-    	
-    }
 
     public String getHandle() {
         return handle;
@@ -80,86 +76,4 @@ public class User implements Serializable {
         }
     }
 
-}
-
-//import java.io.Serializable;
-//import java.util.*;
-//
-//
-//public class User implements Serializable {
-//    private String handle;
-//    private String password;
-//    private ArrayList<User> friends;
-//    private ArrayList<Chat> chats;
-//    private int numFriends;
-//
-//
-//
-//    public User(String handle, String password) {
-//        this.handle = handle;
-//        this.password = password;
-//        friends = new ArrayList<>();
-//        chats = new ArrayList<>();
-//        numFriends = 0;
-//    }
-//
-//    public String getHandle() {
-//        return handle;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void changeHandle(String handle) {
-//        this.handle = handle;
-//    }
-//
-//    public void changePassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public ArrayList<User> getFriends() {
-//        return friends;
-//    }
-//
-//    public void setFriends(ArrayList<User> friends) {
-//        this.friends = friends;
-//    }
-//
-//    public ArrayList<Chat> getChats() {
-//        return chats;
-//    }
-//
-//    public void setChats(ArrayList<Chat> chats) {
-//        this.chats = chats;
-//    }
-//
-//    public void addFriends(User u) {
-//        if(!friends.contains(u)) {
-//            friends.add(u);
-//            numFriends++;
-//        }
-//    }
-//
-//    public void removeFriend(User u) {
-//        //add gui menu here
-//        if(friends.contains(u)) {
-//            friends.remove(u);
-//            numFriends--;
-//            //have gui print confirmation
-//        }
-//    }
-//
-//    public void addChat(Chat c)  {
-//        if(!chats.contains(c)) {
-//            chats.add(c)
-//        }
-//    }
-//    public void removeChat(Chat c) {
-//        if(chats.contains(c)) {
-//            chats.remove(c);
-//        }
-//    }
-//
 }
