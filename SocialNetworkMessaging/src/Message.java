d8ee220715aa7e9ea3109c235d5c723d07502239
ehendrich05111 <68792546+ehendrich05111@users.noreply.github.com>
2020-07-31 11:21:39 -0400

    Add files via upload

--- SocialNetworkMessaging/src/Message.java
+++ SocialNetworkMessaging/src/Message.java
@@ -1,28 +1,46 @@
 import java.io.Serializable;
 
-public class Message implements Serializable {
-    private String handle;
-    private String content;
+/*
+ * Message class consists of two fields, a constructor, and getters and setters. It
+ * represents a message, so it has the message content and the handle of the user who
+ * send the message.
+ * 
+ * <p>Purdue University -- CS18000 -- Summer 2020 -- Project 5 -- Messaging Service</p>
+ * 
+ * @authors Evan Hendrich, Raj Karra
+ * @version July 31, 2020
+ */
 
+public class Message implements Serializable {
+    private String handle; // handle of user who sent the message
+    private String content; // the message sent
+
+    /* 
+     * constructor
+     * 
+     * @param String handle, handle of user sending message
+     * 
+     * @param String content, the message sent
+     */
     public Message(String handle, String content) {
         this.handle = handle;
         this.content = content;
-    }
+    } // Message
 
     public String getHandle() {
         return handle;
-    }
+    } // getHandle
 
     public void setHandle(String handle) {
         this.handle = handle;
-    }
+    } // setHandle
 
     public String getContent() {
         return content;
-    }
+    } // getContent
 
     public void setContent(String content) {
         this.content = content;
-    }
+    } // setContent
 }
 
