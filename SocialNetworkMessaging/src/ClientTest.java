96c77b8f7c3f1f444bb54de832b6c850a948654d
davidlee <leegiridavid@gmail.com>
2020-08-01 01:07:34 +0900

    Client Test

---
+++ SocialNetworkMessaging/src/ClientTest.java
@@ -0,0 +1,20 @@
+/**
+ * A framework to run public test cases.
+ *
+ * <p>Purdue University -- CS18000 -- Summer 2020</p>
+ *
+ * @author Ho Jun Lee
+ * @version July 31, 2020
+ */
+public class RunLocalTest {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestCase.class);
+        if (result.wasSuccessful()) {
+            System.out.println("Excellent - Test ran successfully");
+        } else {
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
+    }
+    public class ClientTest {
